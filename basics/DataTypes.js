139b69034c71bc3c56252e257157405bde65c2da
scarz03 <affanshaikh5988@gmail.com>
2025-05-26 18:05:59 +0000

    some basics done

---
+++ basics/DataTypes.js
@@ -0,0 +1,7 @@
+let name = "affan"
+let age = 14
+let Status = true
+let Data = null
+let Datasize;
+
+console.log(typeof name, typeof age, typeof Status, typeof Data, typeof Datasize)
---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountNum = 111
+var accountBal = 5000
+accountBal = 4000
+let accountCity = "noida"
+
+// dont use var because of scope issue
+
+console.table([accountNum, accountBal, accountCity])
